6ae9442b8408711e9bae6fba10f86419d2cac189
Robert Tieber <robert.tieber@gmail.com>
2019-06-04 19:51:00 +0200

    Implemented model properly

--- src/main/kotlin/at/donrobo/model/DeckbuilderObject.kt
+++ src/main/kotlin/at/donrobo/model/DeckbuilderObject.kt
@@ -1,38 +1,98 @@
 package at.donrobo.model
 
 import at.donrobo.MagicCard
+import javafx.beans.InvalidationListener
 import javafx.beans.property.DoubleProperty
-import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.ReadOnlyProperty
+import javafx.beans.value.ChangeListener
 import java.io.Serializable
+import java.util.*
+import kotlin.collections.HashMap
 
-interface DeckbuilderObject : Serializable
-
-class CardDeckbuilderObject(val card: MagicCard) : DeckbuilderObject
-
-class PositionedObject(val deckbuilderObject: DeckbuilderObject, val container: DeckbuilderContainer) {
-    init {
-        container.addPositionedObject(this)
-    }
-
-    val xProperty: DoubleProperty = SimpleDoubleProperty()
-    val yProperty: DoubleProperty = SimpleDoubleProperty()
-
+data class PositionProperty(val xProperty: DoubleProperty, val yProperty: DoubleProperty) {
     var x: Double
+        get() = xProperty.value
         set(value) {
             xProperty.value = value
         }
-        get() = xProperty.value
-
     var y: Double
+        get() = yProperty.value
         set(value) {
-            xProperty.value = value
+            yProperty.value = value
         }
-        get() = xProperty.value
-
 }
 
-interface PositionedDeckbuilderContainer {
-    val positionedObjects: List<PositionedObject>
+sealed class DeckbuilderObject : Serializable {
+    val uuid: UUID = UUID.randomUUID()
 
-    fun addPositionedObject(positionedObject: PositionedObject)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is DeckbuilderObject) return false
+
+        if (uuid != other.uuid) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return uuid.hashCode()
+    }
+}
+
+data class CardDeckbuilderObject(val card: MagicCard) : DeckbuilderObject()
+
+class CollectionDeckbuilderObject : DeckbuilderObject(), ReadOnlyProperty<Map<DeckbuilderObject, PositionProperty>> {
+
+    private val invalidationListeners: MutableList<InvalidationListener> = ArrayList()
+    private val changeListeners: MutableList<ChangeListener<in Map<DeckbuilderObject, PositionProperty>>> = ArrayList()
+
+    private val internalDeckbuilderObjects: MutableMap<DeckbuilderObject, PositionProperty> = HashMap()
+
+    val deckbuilderObjects: Map<DeckbuilderObject, PositionProperty> get() = internalDeckbuilderObjects
+
+    fun addObject(deckbuilderObject: DeckbuilderObject, position: PositionProperty) {
+        val oldValue = internalDeckbuilderObjects.toMap()
+
+        internalDeckbuilderObjects[deckbuilderObject] = position
+
+        changeListeners.forEach { it.changed(this, oldValue, deckbuilderObjects) }
+    }
+
+    fun removeObject(deckbuilderObject: DeckbuilderObject): PositionProperty? {
+        val oldValue = internalDeckbuilderObjects.toMap()
+
+        val returnValue = internalDeckbuilderObjects.remove(deckbuilderObject)
+
+        changeListeners.forEach { it.changed(this, oldValue, deckbuilderObjects) }
+
+        return returnValue
+    }
+
+    override fun removeListener(listener: ChangeListener<in Map<DeckbuilderObject, PositionProperty>>) {
+        changeListeners.remove(listener)
+    }
+
+    override fun removeListener(listener: InvalidationListener) {
+        invalidationListeners.remove(listener)
+    }
+
+    override fun getName(): String {
+        return "CollectionDeckbuilderObject"
+    }
+
+    override fun addListener(listener: ChangeListener<in Map<DeckbuilderObject, PositionProperty>>) {
+        changeListeners += listener
+    }
+
+    override fun addListener(listener: InvalidationListener) {
+        invalidationListeners += listener
+    }
+
+    override fun getBean(): Any? {
+        return null
+    }
+
+    override fun getValue(): Map<DeckbuilderObject, PositionProperty> {
+        return deckbuilderObjects
+    }
 }
