e316e7a748b565e4ab633d4a3f47320b60ff633a
Robert Tieber <robert.tieber@gmail.com>
2019-06-27 12:24:45 +0200

    Added content to main UI

--- src/main/kotlin/at/donrobo/view/MainUI.kt
+++ src/main/kotlin/at/donrobo/view/MainUI.kt
@@ -1,15 +1,31 @@
 package at.donrobo.view
 
+import at.donrobo.model.CardDeckbuilderObject
+import at.donrobo.model.CollectionDeckbuilderObject
+import at.donrobo.mtg.CardLoader
 import javafx.fxml.FXML
-import javafx.scene.layout.AnchorPane
+import javafx.scene.control.ScrollPane
+import javafx.scene.layout.VBox
 import javafx.stage.Stage
 
 
 class MainUI {
+
+    private var rootDeckbuilderView = DeckbuilderView(CollectionDeckbuilderObject())
+
     @FXML
-    lateinit var apMain: AnchorPane
+    lateinit var vbMain: VBox
+
+    @FXML
+    lateinit var spMainScroller: ScrollPane
 
     fun close() {
-        (apMain.scene.window as Stage).close()
+        (vbMain.scene.window as Stage).close()
+    }
+
+    @FXML
+    fun initialize() {
+        spMainScroller.content = rootDeckbuilderView
+        rootDeckbuilderView.deckbuilderCollection.addObject(CardDeckbuilderObject(CardLoader().randomCard()))
     }
 }
